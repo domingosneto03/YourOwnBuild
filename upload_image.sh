b931fc5b2c900423e885942cf9e4ff594b9413db
Domingos Neto <domingos12126@gmail.com>
2023-11-16 17:37:58 +0000

    ficheiros corretos

--- upload_image.sh
+++ upload_image.sh
@@ -3,7 +3,7 @@
 # Stop execution if a step fails
 set -e
 
-IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbawYYYY/lbawYYXX # Replace with your group's image name
+IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2324/lbaw23104 # Replace with your group's image name
 
 # Ensure that dependencies are available
 composer install
