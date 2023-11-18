08130db88f1d2b857308e2305f7dc7dad16599fe
Domingos Manuel OrdenAC/ Neto <up202108728@fe.up.pt>
2023-11-18 03:37:44 +0000

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,54 +2,56 @@
 -- Drop old schema
 -----------------------------------------
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS task; 
-DROP TABLE IF EXISTS comment; 
-DROP TABLE IF EXISTS project;
-DROP TABLE IF EXISTS member;
-DROP TABLE IF EXISTS admins;
-DROP TABLE IF EXISTS blocked;
-DROP TABLE IF EXISTS notifications;
-DROP TABLE IF EXISTS priority;
-DROP TABLE IF EXISTS responsible;
-DROP TABLE IF EXISTS everyone;
-DROP TABLE IF EXISTS creator;
-DROP TABLE IF EXISTS invited;
-DROP TABLE IF EXISTS request_join;
-DROP TABLE IF EXISTS user_comment;
-DROP TABLE IF EXISTS task_notification;
-DROP TABLE IF EXISTS comment_notification;
-DROP TABLE IF EXISTS user_notification;
-DROP TABLE IF EXISTS project_notification;
+DROP TABLE IF EXISTS users2 CASCADE;
+DROP TABLE IF EXISTS task CASCADE; 
+DROP TABLE IF EXISTS comment CASCADE; 
+DROP TABLE IF EXISTS project CASCADE;
+DROP TABLE IF EXISTS member CASCADE;
+DROP TABLE IF EXISTS notifications CASCADE;
+DROP TABLE IF EXISTS responsible CASCADE;
+DROP TABLE IF EXISTS invited CASCADE;
+DROP TABLE IF EXISTS request_join CASCADE;
+DROP TABLE IF EXISTS task_notification CASCADE;
+DROP TABLE IF EXISTS comment_notification CASCADE;
+DROP TABLE IF EXISTS user_notification CASCADE;
+DROP TABLE IF EXISTS project_notification CASCADE;
+DROP TABLE IF EXISTS admin_action CASCADE;
 
 DROP TYPE IF EXISTS task_notification_types;
 DROP TYPE IF EXISTS comment_notification_types;
-DROP TYPE IF EXISTS user_comment_notification_types;
+DROP TYPE IF EXISTS user_notification_types;
 DROP TYPE IF EXISTS project_notification_types;
 DROP TYPE IF EXISTS member_types;
 DROP TYPE IF EXISTS priority_types;
 
-DROP FUNCTION IF EXISTS project_search_update;
-DROP FUNCTION IF EXISTS task_search_update;
-DROP FUNCTION IF EXISTS user_search_update;
-DROP FUNCTION IF EXISTS comment_search_update;
-DROP FUNCTION IF EXISTS check_task_dates;
-DROP FUNCTION IF EXISTS check_project_invitation;
+DROP FUNCTION IF EXISTS project_search_update CASCADE;
+DROP FUNCTION IF EXISTS task_search_update CASCADE;
+DROP FUNCTION IF EXISTS user_search_update CASCADE;
+DROP FUNCTION IF EXISTS comment_search_update CASCADE;
+DROP FUNCTION IF EXISTS check_task_dates CASCADE;
+DROP FUNCTION IF EXISTS check_project_invitation CASCADE;
+DROP FUNCTION IF EXISTS default_project_coordinator CASCADE;
+DROP FUNCTION IF EXISTS notify_users_on_leave CASCADE;
+DROP FUNCTION IF EXISTS block_user CASCADE;
 
-DROP TRIGGER IF EXISTS project_search_update_trigger ON project;
-DROP TRIGGER IF EXISTS task_search_update_trigger ON task;
-DROP TRIGGER IF EXISTS user_search_update_trigger ON users;
-DROP TRIGGER IF EXISTS comment_search_update_trigger ON comment;
-DROP TRIGGER IF EXISTS trigger_check_task_dates ON task;
-DROP TRIGGER IF EXISTS trigger_default_project_coordinator ON project;
-DROP TRIGGER IF EXISTS tr_notify_users_on_leave ON member;
+DROP TRIGGER IF EXISTS project_search_update_trigger ON project CASCADE;
+DROP TRIGGER IF EXISTS task_search_update_trigger ON task CASCADE;
+DROP TRIGGER IF EXISTS user_search_update_trigger ON users2 CASCADE;
+DROP TRIGGER IF EXISTS comment_search_update_trigger ON comment CASCADE;
+DROP TRIGGER IF EXISTS trigger_check_task_dates ON task CASCADE;
+DROP TRIGGER IF EXISTS trigger_default_project_coordinator ON project CASCADE;
+DROP TRIGGER IF EXISTS tr_notify_users_on_leave ON member CASCADE;
+DROP TRIGGER IF EXISTS trigger_block_user ON admin_action CASCADE;
+DROP TRIGGER IF EXISTS trigger_check_project_invitation ON member CASCADE;
 
-DROP INDEX IF EXISTS idx_user_notification;
-DROP INDEX IF EXISTS idx_user_comment;
-DROP INDEX IF EXISTS search_project_idx;
-DROP INDEX IF EXISTS search_task_idx;
-DROP INDEX IF EXISTS search_user_idx;
-DROP INDEX IF EXISTS search_comment_idx;
+
+DROP INDEX IF EXISTS idx_user_notification CASCADE;
+DROP INDEX IF EXISTS idx_user_comment CASCADE;
+DROP INDEX IF EXISTS idx_priority_task CASCADE;
+DROP INDEX IF EXISTS search_project_idx CASCADE;
+DROP INDEX IF EXISTS search_task_idx CASCADE;
+DROP INDEX IF EXISTS search_user_idx CASCADE;
+DROP INDEX IF EXISTS search_comment_idx CASCADE;
 
 
 -----------------------------------------
@@ -67,28 +69,33 @@ CREATE TYPE priority_types AS ENUM ('low', 'medium', 'high');
 -- Tables
 -----------------------------------------
 
-CREATE TABLE users (
+CREATE TABLE users2 (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
-    bio TEXT
+    bio TEXT,
+    is_admin BOOLEAN NOT NULL DEFAULT false,
+    is_blocked BOOLEAN NOT NULL DEFAULT false
 );
 
 
 CREATE TABLE task (
     id SERIAL PRIMARY KEY,
-    id_creator INT NOT NULL REFERENCES users(id),
+    id_creator INT NOT NULL REFERENCES users2(id),
+    id_project INT NOT NULL REFERENCES project(id),
     name VARCHAR(255) NOT NULL,
     label VARCHAR(255) NOT NULL,
     is_completed BOOLEAN NOT NULL DEFAULT false,
     date_created DATE NOT NULL,
-    due_date DATE NOT NULL CHECK (due_date >= date_created)
+    due_date DATE NOT NULL CHECK (due_date >= date_created),
+    priority priority_types NOT NULL
 );
 
 CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
+    id_user INT NOT NULL REFERENCES users2(id),
     id_task INT NOT NULL REFERENCES task(id),
     content TEXT NOT NULL,
     date TIMESTAMP NOT NULL
@@ -96,6 +103,7 @@ CREATE TABLE comment (
 
 CREATE TABLE project (
     id SERIAL PRIMARY KEY,
+    id_creator INT NOT NULL REFERENCES users2(id),
     name VARCHAR(255) NOT NULL,
     description TEXT,
     is_public BOOLEAN NOT NULL DEFAULT false,
@@ -104,69 +112,34 @@ CREATE TABLE project (
 
 CREATE TABLE member (
     id SERIAL PRIMARY KEY,
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     id_project INT NOT NULL REFERENCES project(id),
-    is_promoted BOOLEAN NOT NULL DEFAULT false,
-    member_type member_types NOT NULL
-);
-
-CREATE TABLE admins (
-    id SERIAL PRIMARY KEY,
-    id_user INT NOT NULL REFERENCES users(id)
-);
-
-CREATE TABLE blocked (
-    id SERIAL PRIMARY KEY,
-    id_user INT NOT NULL REFERENCES users(id)
+    role member_types NOT NULL
 );
 
 CREATE TABLE notifications (
     id SERIAL PRIMARY KEY,
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     date TIMESTAMP NOT NULL,
     seen BOOLEAN NOT NULL DEFAULT false
 );
 
-CREATE TABLE priority (
-    id SERIAL PRIMARY KEY,
-    id_task INT NOT NULL REFERENCES task(id),
-    priority_type priority_types NOT NULL
-);
-
 CREATE TABLE responsible (
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     id_task INT NOT NULL REFERENCES task(id),
     PRIMARY KEY (id_user, id_task)
 );
 
-CREATE TABLE everyone (
-    id_user INT NOT NULL REFERENCES users(id),
-    id_notification INT NOT NULL REFERENCES notifications(id),
-    PRIMARY KEY (id_user, id_notification)
-);
-
-CREATE TABLE creator (
-    id_user INT NOT NULL REFERENCES users(id),
-    id_project INT NOT NULL REFERENCES project(id),
-    PRIMARY KEY (id_user, id_project)
-);
-
 CREATE TABLE invited (
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     id_project INT NOT NULL REFERENCES project(id),
     PRIMARY KEY (id_user, id_project)
 );
 
 CREATE TABLE request_join (
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     id_project INT NOT NULL REFERENCES project(id),
     PRIMARY KEY (id_user, id_project)
-);
-
-CREATE TABLE user_comment (
-    id_user INT NOT NULL REFERENCES users(id),
-    id_comment INT NOT NULL REFERENCES comment(id),
-    PRIMARY KEY (id_user, id_comment)
 );
 
 CREATE TABLE task_notification (
@@ -183,7 +156,7 @@ CREATE TABLE comment_notification (
 
 CREATE TABLE user_notification (
     id INT PRIMARY KEY REFERENCES notifications (id),
-    id_user INT NOT NULL REFERENCES users(id),
+    id_user INT NOT NULL REFERENCES users2(id),
     notification_type user_notification_types NOT NULL
 );
 
@@ -191,6 +164,14 @@ CREATE TABLE project_notification (
     id INT PRIMARY KEY REFERENCES notifications (id),
     id_project INT NOT NULL REFERENCES project(id),
     notification_type project_notification_types NOT NULL
+);
+
+CREATE TABLE admin_action (
+    id SERIAL PRIMARY KEY,
+    id_admin INT NOT NULL REFERENCES users2(id),
+    id_user INT NOT NULL REFERENCES users2(id),
+    is_blocking BOOLEAN NOT NULL,
+    justification TEXT NOT NULL
 );
 
 --IDX01
@@ -202,6 +183,9 @@ CLUSTER notifications USING idx_user_notification;
 CREATE INDEX idx_user_comment ON comment USING btree (id_task);   
 
 CLUSTER comment USING idx_user_comment;
+
+--IDX03
+CREATE INDEX idx_priority_task ON task (is_completed, priority);
 
 --IDX03
 -- Add column to group to store computed ts_vectors.
@@ -277,7 +261,7 @@ CREATE INDEX search_task_idx ON task USING GIN (tsvectors);
 
 --IDX05
 -- Add column to user to store computed ts_vectors.
-ALTER TABLE users
+ALTER TABLE users2
 ADD COLUMN tsvectors TSVECTOR;
 
 -- Create a function to automatically update ts_vectors.
@@ -301,13 +285,13 @@ LANGUAGE plpgsql;
 
 -- Create a trigger before insert or update on user.
 CREATE TRIGGER user_search_update_trigger
- BEFORE INSERT OR UPDATE ON users
+ BEFORE INSERT OR UPDATE ON users2
  FOR EACH ROW
  EXECUTE PROCEDURE user_search_update();
 
 
 -- Finally, create a GIN index for ts_vectors.
-CREATE INDEX search_user_idx ON users USING GIN (tsvectors);
+CREATE INDEX search_user_idx ON users2 USING GIN (tsvectors);
 
 --IDX06
 -- Add column to comment to store computed ts_vectors.
@@ -431,22 +415,24 @@ AFTER DELETE ON member
 FOR EACH ROW
 EXECUTE FUNCTION notify_users_on_leave();
 
---TRANSACTION01
-BEGIN TRANSACTION;
+--TRIGGER05
+-- Step 1: Define the trigger function
+CREATE OR REPLACE FUNCTION block_user() RETURNS TRIGGER AS $$
+BEGIN
+    -- Check if the action is to block the user
+    IF NEW.is_blocking THEN
+        -- Update the users table to set is_blocked to true for the user
+        UPDATE users2 SET is_blocked = true WHERE id = NEW.id_user;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
-
--- Insert the new project into the `project` table
-INSERT INTO project (name, description, is_public, date_created)
-VALUES ($project_name, $project_description, $project_visibility, NOW());
-
--- Add the creator of the project as the default coordinator in the `member` table with `is_promoted` set to `true`
-INSERT INTO member (id_user, id_project, is_promoted, member_types)
-VALUES ($creator_id, currval('project_id_seq'), true, 'coordinator');
-
--- Add a record in the `creator` table linking the user to the project
-INSERT INTO creator (id_user, id_project)
-VALUES ($creator_id, currval('project_id_seq'));
+-- Step 2: Create the trigger
+CREATE TRIGGER trigger_block_user
+AFTER INSERT ON admin_action
+FOR EACH ROW
+EXECUTE FUNCTION block_user();
 
 -- Send a notification to the creator confirming the successful creation of the project
 INSERT INTO notifications (id_user, date, seen)
