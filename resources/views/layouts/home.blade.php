4575241d7f9bec6ed1a3e4c7a7bcc01056fdc0cd
Tomas Camara <up202108665@fe.up.pt>
2023-11-19 20:33:35 +0000

    grid working on homepage

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Dashboard</title>
-    <!-- Add your stylesheet links here -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ url('css/home.css') }}" rel="stylesheet">
+    <script type="text/javascript">
+        // Fix for Firefox autofocus CSS bug
+        // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
+    </script>
+    <script type="text/javascript" src={{ url('js/app.js') }} defer>
+    </script>
 </head>
 <body>
+
 <div class="container">
 
     <!-- Search and profile icons -->
