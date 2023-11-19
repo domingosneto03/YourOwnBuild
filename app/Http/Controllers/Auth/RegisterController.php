d7a6945b3fdba4c73a91bb4c7a09fe2f9b884bf7
Tomas Camara <up202108665@fe.up.pt>
2023-11-19 08:35:11 +0000

    fixed some bugs

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,12 +29,18 @@ class RegisterController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:250',
-            'email' => 'required|email|max:250|unique:users',
+            'email' => [
+                'required',
+                'email',
+                'max:250',
+                Rule::unique(User::class), // Use the unique rule with the User model
+            ],
             'password' => 'required|min:8|confirmed'
         ]);
+        
 
         User::create([
-            'name' => $request->name,
+            'username' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
---
+++ run_locally.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Stop execution if a step fails
+set -e
+
+IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2324/lbaw23104 # Replace with your group's image name
+
+# Ensure that dependencies are available
+composer install
+php artisan config:clear
+php artisan clear-compiled
+php artisan optimize
+
+# docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
+
+docker rm lbaw23104 || true
+
+docker run -it -p 8000:80 --name=lbaw23104 -e DB_DATABASE="lbaw23104" -e DB_SCHEMA="lbaw23104" -e DB_USERNAME="lbaw23104" -e DB_PASSWORD="lmnaKqEy" -e APP_DEBUG=true -e APP_LOG_LEVEL=debug -e APP_URL="http://localhost:8000" -e ASSET_URL="http://localhost:8000" -e FORCE_HTTPS=false  git.fe.up.pt:5050/lbaw/lbaw2324/lbaw23104
