0667c45511021e9fd5adc49782b1e1aa7c852208
Tomas Camara <up202108665@fe.up.pt>
2023-11-20 16:32:29 +0000

    Made project page more similar to wireframe

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -5,27 +5,27 @@
 <section class="tasks-list">
     @php 
         $tasks = $project->tasks()->get();
+        $pendingTasks = $tasks->where('completion', 'pending');
+        $inProgressTasks = $tasks->where('completion', 'in_progress');
+        $completedTasks = $tasks->where('completion', 'completed');
     @endphp
-    @foreach ($tasks as $task)
-        <!-- Project Card Link -->
-        <a href="/task/{{ $task->id }}" class="task-card-link"> <!-- Update href with the URL to the project page -->
-            <div class="task-card">
-                <div class="card-header">
-                    <h3>{{ $task->name }}</h3>
-                    <p>Creator Name: {{ $project->creator->name }}</p>
-                </div>
-                <div class="card-body">
-                    <p>{{ $task->label }}</p>
-                </div>
-                <div class="card-footer">
-                    @if ($task->is_completed)
-                        <p>The task has been finished.</p>
-                    @else
-                        <p>The task is not finished.</p>
-                    @endif
-                </div>
-            </div>
-        </a>
-    @endforeach
+    <div class="task-category">
+        <h2>Pending</h2>
+        @foreach ($pendingTasks as $task)
+            @include('partials.task', ['task' => $task])
+        @endforeach
+    </div>
+    <div class="task-category">
+        <h2>In Progress</h2>
+        @foreach ($inProgressTasks as $task)
+            @include('partials.task', ['task' => $task])
+        @endforeach
+    </div>
+    <div class="task-category">
+        <h2>Completed</h2>
+        @foreach ($completedTasks as $task)
+            @include('partials.task', ['task' => $task])
+        @endforeach
+    </div>
 </section>
 @endsection
---
+++ resources/views/partials/task.blade.php
@@ -0,0 +1,15 @@
+<!-- Project Card Link -->
+<a href="/task/{{ $task->id }}" class="task-card-link"> <!-- Update href with the URL to the project page -->
+    <div class="task-card">
+        <div class="card-header">
+            <h3>{{ $task->name }}</h3>
+            <p>Creator Name: {{ $project->creator->name }}</p>
+        </div>
+        <div class="card-body">
+            <p>{{ $task->label }}</p>
+        </div>
+        <div class="card-footer">
+            <p>The task is {{ $task->label }}</p>
+        </div>
+    </div>
+</a>
