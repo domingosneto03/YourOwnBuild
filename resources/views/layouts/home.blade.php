812adf1376566ac80836025bd03c6b7eb32699d1
Tomas Camara <up202108665@fe.up.pt>
2023-11-27 16:40:49 +0000

    Fixed sidebar

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -35,32 +35,30 @@
             <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li class="nav-item">
-            <a class="nav-link" href="../logout">Logout</a>
-            </li>
         </ul>
         <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
-        <div class="user-profile">
+        <div class="dropdown">
             @php 
                 $user = Auth::user();
             @endphp
-            <a href="/user/{{ $user->id }}"> <!-- Update the href value to the actual profile page URL -->
-                <img src="{{ asset('img/profile_pic.jpeg') }}" alt="User Profile" style="width: 50px; height: 50px; border-radius: 50%;">
+            <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{ asset('img/profile_pic.jpeg') }}" alt="" width="32" height="32" class="rounded-circle me-2">
+                <strong>{{ $user->name }}</strong>
             </a>
+            <ul class="dropdown-menu text-small shadow">
+                <li><a class="dropdown-item" href="/user/{{ $user->id }}">Profile</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="../logout">Sign out</a></li>
+            </ul>
         </div>
         </div>
     </div>
     </nav>
 
     @yield('content')
-    <footer>
-        <a href="#">About Us</a>
-        <a href="#">FAQ</a>
-        <a href="#">Help</a>
-    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.home')
 
 @section('content')
-<!-- Sidebar -->
-<div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
+<main class="d-flex">
+  <!-- Sidebar -->
+  <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px;">
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
@@ -41,41 +38,27 @@
       </li>
     </ul>
     <hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
   </div>
 
-<!-- Projects List -->
-<main class="main-content">
-    <section class="projects-list">
-        @foreach ($projects as $project)
-            <!-- Project Card Link -->
-            <a href="/project/{{ $project->id }}" class="project-card-link"> <!-- Update href with the URL to the project page -->
-                <div class="project-card">
-                    <div class="card-header">
-                        <h3>{{ $project->name }}</h3>
-                        <p>Creator Name: {{ $project->creator->name }}</p>
-                    </div>
-                    <div class="card-body">
-                        <p>{{ $project->description }}</p>
-                    </div>
-                    <div class="card-footer">
-                        <span>{{ count($project->members) }} members</span>
-                    </div>
-                </div>
-            </a>
-        @endforeach
-    </section>
+  <!-- Projects List -->
+  <section class="projects-list">
+      @foreach ($projects as $project)
+          <!-- Project Card Link -->
+          <a href="/project/{{ $project->id }}" class="project-card-link"> <!-- Update href with the URL to the project page -->
+              <div class="project-card">
+                  <div class="card-header">
+                      <h3>{{ $project->name }}</h3>
+                      <p>Creator Name: {{ $project->creator->name }}</p>
+                  </div>
+                  <div class="card-body">
+                      <p>{{ $project->description }}</p>
+                  </div>
+                  <div class="card-footer">
+                      <span>{{ count($project->members) }} members</span>
+                  </div>
+              </div>
+          </a>
+      @endforeach
+  </section>
 </main>
 @endsection
