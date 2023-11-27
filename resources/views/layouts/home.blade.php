76b7bd613233abe27500e798c6282f1a1ca34e57
Tomas Camara <up202108665@fe.up.pt>
2023-11-27 15:27:33 +0000

    navbar bootstrap

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,10 +11,10 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ url('css/home.css') }}" rel="stylesheet">
     <link href="{{ url('css/homepage.css') }}" rel="stylesheet">
     <link href="{{ url('css/project.css') }}" rel="stylesheet">
     <link href="{{ url('css/create.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
         // See: http://stackoverflow.com/questions/18943276/html-5-autofocus-messes-up-css-loading/18945951#18945951
@@ -24,15 +24,24 @@
     <script type="text/javascript" src={{ url('js/search.js') }} defer></script>
 </head>
 <body>
-
-<div class="container">
-    <!-- Topnav bar -->
-    <div class="topnav">
-        <a class="active" href="../homepage/">YourOwnBuild</a>
-        <!-- <input type="text" placeholder="Search.."> -->
-        <form>
-            <input type="text" size="30" onkeyup="showResult(this.value)" placeholder="Search users..">
-            <div id="livesearch"></div>
+    <nav class="navbar navbar-expand-md mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">YourOwnBuild</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="../logout">Logout</a>
+            </li>
+        </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
         <div class="user-profile">
             @php 
@@ -42,14 +51,16 @@
                 <img src="{{ asset('img/profile_pic.jpeg') }}" alt="User Profile" style="width: 50px; height: 50px; border-radius: 50%;">
             </a>
         </div>
+        </div>
     </div>
+    </nav>
 
-        @yield('content')
-        <footer>
-            <a href="#">About Us</a>
-            <a href="#">FAQ</a>
-            <a href="#">Help</a>
-        </footer>
-    </div>
+    @yield('content')
+    <footer>
+        <a href="#">About Us</a>
+        <a href="#">FAQ</a>
+        <a href="#">Help</a>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
