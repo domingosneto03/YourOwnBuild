88f505fedb075854fc222258fae8f74e27604f17
Domingos Neto <domingos12126@gmail.com>
2023-12-07 17:47:38 +0000

    edit project display fixed

--- resources/views/pages/editproject.blade.php
+++ resources/views/pages/editproject.blade.php
@@ -5,27 +5,22 @@
         @csrf
         @method('PUT')
 
-        <!-- Text input -->
-        <div data-mdb-input-init class="form-outline mb-4">
-            <input type="text" id="name" class="form-control" />
-            <label class="form-label" for="name" value="{{ $project->name }}" required class="input">Project Name</label>
+        <div class="mb-3 col-8">
+            <label for="name" class="form-label">Project Name:</label>
+            <input class="form-control" type="text" name="name" id="name" value="{{ $project->name }}" required>
         </div>
-
-        <!-- Message input -->
-        <div data-mdb-input-init class="form-outline mb-4">
-            <textarea class="form-control" id="description" rows="4">{{ $project->description }}</textarea>
-            <label class="form-label" for="description">Description</label>
+        <div class="mb-3 col-8">
+            <label for="description" class="form-label">Description:</label>
+            <textarea class = "form-control" name="description" id="description">{{ $project->description }}</textarea>
         </div>
-
-        <div data-mdb-input-init class="form-outline mb-4">
-            <select class='selection' name="is_public" id="is_public">
-                <option value="0" {{ $project->is_public === 0 ? 'selected' : '' }}>Private</option>
-                <option value="1" {{ $project->is_public === 1 ? 'selected' : '' }}>Public</option>
+        <div class="mb-3 col-8">
+            <select class="form-select" aria-label="Privacy" name = "is_public">
+              <option value="0" {{ $project->is_public === 0 ? 'selected' : '' }}>Private</option>
+              <option value="1" {{ $project->is_public === 1 ? 'selected' : '' }}>Public</option>
             </select>
-            <label class="form-label" for="is_public">Privacy</label>
         </div>
-
-        <!-- Submit button -->
-        <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Update</button>
+        <div class="col-1">
+            <button class="btn btn-primary btn-sm" type="submit">Update</button>
+        </div>
     </form>
 </div>
