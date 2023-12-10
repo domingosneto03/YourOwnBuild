70a9e05286f9f36e8c97406314de6b2ccc915fe4
Domingos Neto <domingos12126@gmail.com>
2023-12-10 00:53:53 +0000

    Comments work properly, still some design and optimization adjustments to make

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,6 +22,7 @@
     <script type="text/javascript" src={{ url('js/app.js') }} defer></script>
     <script type="text/javascript" src={{ url('js/search.js') }} defer></script>
     <script type="text/javascript" src={{ url('js/drag.js') }} defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="text/javascript" src={{ url('js/comment.js') }} defer></script>
 </head>
 <body class="d-flex flex-column vh-100 m-0">
